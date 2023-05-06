8833f21cc4ac1eee9f64efa95fc70a4127a98e5f
zubair7384 <zubair.iqbal2018@gmail.com>
2023-05-07 00:08:06 +0400

    new

--- app.vue
+++ app.vue
@@ -5,3 +5,19 @@
     </NuxtLayout>
   </div>
 </template>
+
+<script setup lang="ts">
+useHead({
+  title: "Aliens Among Us by Rameez - Intergalactic Fashion Campaign",
+  meta: [
+    {
+      name: "description",
+      content:
+        "Aliens Among Us is an AI generated concept fashion campaign featuring Louis Vuitton's luxury handbag collection by Rameez Iqbal. Featured in designboom.",
+    },
+  ],
+  bodyAttrs: {
+    class: "test",
+  },
+});
+</script>
